fd20b13dabc1464505671fb72f3a94bb7cad7a33
Jason Bakker <jason@ghostpattern.net>
2021-09-12 15:24:45 +1000

    test commit

---
+++ RMITMMO/Assets/CatBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CatBehaviour : MonoBehaviour
+{
+    public Animator Animator;
+    public float CatScareDistance = 2.0f;
+
+    private FirstPersonDrifter _player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = FindObjectOfType<FirstPersonDrifter>();
+        _player = GameObject.FindWithTag("MyPlayer").GetComponent<FirstPersonDrifter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vecToPlayer = _player.transform.position - transform.position;
+
+        vecToPlayer.y = 0.0f;
+
+        float distToPlayer = vecToPlayer.magnitude;
+
+        if(distToPlayer < CatScareDistance)
+        {
+            Animator.SetBool("Walking", true);
+        }
+        else
+        {
+            Animator.SetBool("Walking", false);
+        }
+    }
+}
---
+++ RMITMMO/Assets/FaceCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vecToCamera = Camera.main.transform.position - transform.position;
+        vecToCamera.y = 0.0f;
+        transform.rotation = Quaternion.LookRotation(vecToCamera, Vector3.up);
+    }
+}
